0251552f8a08ccd2e3bd76e6b373e474acadf4e2
labr <labr@paypal.com>
2016-06-29 16:31:15 +0530

    :gem: header design using flex

--- public/js/components/header.js
+++ public/js/components/header.js
@@ -3,7 +3,40 @@ var React = require('react');
 var Header = React.createClass({
     render: function render() {
         return (
-            <h1>Hello World</h1>
+            <div className="header-container">
+                <div className="feather-icon-container">
+                    <div className="feather-icon">
+                        <i className="fa fa-bars" aria-hidden="true"></i>
+                    </div>
+                </div>
+                <div className="bell-icon-container">
+                    <div className="bell-icon">
+                        <i className="fa fa-bell" aria-hidden="true"></i>
+                    </div>
+                </div>
+                <div className="email-icon-container">
+                    <div className="email-icon">
+                        <i className="fa fa-envelope" aria-hidden="true"></i>
+                    </div>
+                </div>
+                <div className="hash-icon-container">
+                    <div className="hash-icon">
+                        <i className="fa fa-hashtag" aria-hidden="true"></i>
+                    </div>
+                </div>
+                <div className="twitter-icon-container">
+                    <div className="twitter-icon">
+                        <i className="fa fa-twitter fa-3x" aria-hidden="true"></i>
+                    </div>
+                </div>
+                <div className="search-bar-container">
+                    <input type="text" className="search-bar" placeholder="Search"/>
+                </div>
+                <div className="display-picture-container">
+                    <div className="display-picture">
+                    </div>
+                </div>
+            </div>
         );
     }
 });
